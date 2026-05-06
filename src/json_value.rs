4a10b7786737eb27eaad27a7fbc1d5c795851169
kyomoto <2028566723@qq.com>
2026-05-06 13:14:55 +0800

    src: json value display trait

--- src/json_value.rs
+++ src/json_value.rs
@@ -1,11 +1,42 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
 
 #[derive(Debug)]
 pub enum JsonValue {
-    Monostate,
-    Double(f64),
+    Null,
+    Number(f64),
     String(String),
     Bool(bool),
     Array(Vec<JsonValue>),
     Object(HashMap<String, JsonValue>),
+}
+
+impl fmt::Display for JsonValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            JsonValue::Null => write!(f, "null"),
+            JsonValue::Number(num) => write!(f, "{}", num),
+            JsonValue::String(s) => write!(f, "\"{}\"", s),
+            JsonValue::Bool(booler) => write!(f, "{}", booler),
+            JsonValue::Array(arr) => {
+                write!(f, "[")?;
+                for (i, v) in arr.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, ",")?;
+                    }
+                    write!(f, "{}", v)?;
+                }
+                write!(f, "]")
+            }
+            JsonValue::Object(obj) => {
+                write!(f, "{{")?;
+                for (i, (k, v)) in obj.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, ",")?;
+                    }
+                    write!(f, "\"{}\":{}", k, v)?;
+                }
+                write!(f, "}}")
+            }
+        }
+    }
 }
