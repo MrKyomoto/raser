1aa112b6c2ef2be6928b40a8d7a79bd210e2de16
kyomoto <2028566723@qq.com>
2026-05-06 16:43:32 +0800

    src: test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+mod json_error;
 mod json_parser;
 mod json_value;
+mod lexer;
 
+use json_error::*;
 use json_parser::*;
 #[warn(unused_imports)]
 use json_value::*;
+use lexer::*;
 
-fn main() {
-    let json_parser = JsonParser::build("test".to_string());
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = r#"{"name":"raser","version":1.0,"ok":true,"list":[1,2,3],"data":null}"#;
+
+    let json = JsonParser::parse_json(input)?;
+    println!("{:#?}", json);
+    Ok(())
 }
